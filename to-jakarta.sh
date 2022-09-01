4cc9688fcb4436f782d606b02afd456276b6840d
Roberto Cortez <radcortez@yahoo.com>
2022-09-01 17:40:29 +0100

    Update Parent POM to 36

--- to-jakarta.sh
+++ to-jakarta.sh
@@ -5,4 +5,5 @@ sed -i '' 's/smallrye-parent/smallrye-jakarta-parent/g' pom.xml
 find . -type f -name '*.java' -exec sed -i '' 's/javax./jakarta./g' {} +
 
 mvn build-helper:parse-version versions:set -DnewVersion=\${parsedVersion.nextMajorVersion}.0.0-SNAPSHOT
+mvn -ntp versions:update-property -Dproperty=version.smallrye.common -DnewVersion=2.0.0-RC1
 
